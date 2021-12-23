cf4981bbaae5e450124fb962910645c49338f245
darin277117 <95065065+darin277117@users.noreply.github.com>
2021-12-24 03:04:42 +0900

    Add files via upload

---
+++ 3장 연습문제/3장 Q4.py
@@ -0,0 +1,5 @@
+i=1
+
+for i in range(1,101) :
+    print(i)
+    
---
+++ 3장 연습문제/3장 Q5.py
@@ -0,0 +1,7 @@
+list=[70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+score=0
+
+for i in list :
+    score=score+i
+    
+print(score/len(list))
